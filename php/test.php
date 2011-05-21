b4e0d388fc0777436c34dfc312494ef059555db9
David Rogers <david@ethos-development.com>
2011-05-21 00:51:01 -0400

    Setting a better baseline for the PHP scripts.

---
+++ php/test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'PHPUnit/Framework.php';
+require_once 'main.php';
+
+class Test extends PHPUnit_Framework_TestCase
+{
+    function test_something ( )
+    {
+        $this->assertTrue(false);
+    }
+}
